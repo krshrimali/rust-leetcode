9e535261b7c87bf35bc490e2780da55e6f5e3267
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-11-08 19:49:03 +0530

    use iter() + buy and sell stock soln

---
+++ src/lc_004_best_time_to_buy_and_sell_stock.rs
@@ -0,0 +1,30 @@
+// it's a maximum difference between 2 elements problem
+// with a condition that the 2 elements should be: a..b and considering b..a is not allowed
+use std::cmp;
+
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    let mut max_diff = 0;
+    let mut min_price = i32::MAX;
+
+    for price in prices.iter() {
+        min_price = cmp::min(min_price, *price);
+        max_diff = cmp::max(*price - min_price, max_diff);
+    }
+
+    max_diff
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        assert_eq!(max_profit([7, 6, 4, 3, 1].to_vec()), 0);
+    }
+
+    #[test]
+    fn ex2() {
+        assert_eq!(max_profit([7, 1, 5, 3, 6, 4].to_vec()), 5);
+    }
+}
